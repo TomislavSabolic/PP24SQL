8f53507dd0bd3a46c789f4c312a261bab56cc79b
TomislavSabolic <37838708+TomislavSabolic@users.noreply.github.com>
2021-11-17 18:41:33 +0100

    Dodao charset i novi unos

--- githubprimjer.sql
+++ githubprimjer.sql
@@ -1,4 +1,9 @@
 drop database if exists githubprimjer;
-create database githubprimjer;
+create database githubprimjer charachter set utf8;
 use githubprimmjer;
-create table git()
+create table git(
+sifra int not null primary key auto_increment
+
+)
+
+insert into git(sifra,naziv)values (null,'Test');
