b3b0fde6a6f3f568eaa5589d65c5f2154b298fea
TomislavSabolic <37838708+TomislavSabolic@users.noreply.github.com>
2021-11-20 21:08:16 +0100

    nadogradnja

--- githubprimjer.sql
+++ githubprimjer.sql
@@ -2,8 +2,9 @@ drop database if exists githubprimjer;
 create database githubprimjer charachter set utf8;
 use githubprimmjer;
 create table git(
-sifra int not null primary key auto_increment
+sifra int not null primary key auto_increment,
+naziv varchar(50) not null
 
-)
+);
 
 insert into git(sifra,naziv)values (null,'Test');
